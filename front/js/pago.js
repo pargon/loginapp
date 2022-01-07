7268315a7bac61f1806f2157091ee16f49e3bca3
Gonzalo Parra <gparra@VALKIMIA.COM>
2022-01-06 21:24:19 -0300

    front con paypal

--- front/js/pago.js
+++ front/js/pago.js
@@ -1,44 +1,44 @@
 const base_url = "http://localhost:3000"
 
-var payment_btn = document.getElementById("payment_btn");
-payment_btn.addEventListener('click', get_preference_id);
+var mercadopago_btn = document.getElementById("mercadopago");
+mercadopago_btn.addEventListener('click', get_preference_mercadopago);
 
-function get_preference_id(e) {
+function get_preference_mercadopago(e) {
   // paso 1. Preparar el pago (ir al backend y obtener un preference_id)
   // paso 2. Crear un botón que abre la ventana de MercadoPago.
   e.preventDefault();
   console.log("click");
 
-  const payment_url = `${base_url}/pago`;
+  const payment_url = `${base_url}/mercadopago/pago`;
   let MERCADOPAGO_PUBLIC_KEY = 'TEST-19a131dc-b914-4f5f-843b-87384f2ae4e4';
 
-  const data = {"amount": 1234}
+  const data = { "amount": 1234 }
 
   fetch(payment_url, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data),
-    }
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }
   )
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const preference_id = data.preference_id;
-    const url = data.url;
-    const redirect = true;  // change this to have different views
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const preference_id = data.preference_id;
+      const url = data.url;
+      const redirect = true;  // change this to have different views
 
-    if (redirect){
-      // use the URL if you want to redirect
-      console.log(`Redireccionar a la url: ${url}`)
-      window.location.href = url;
-    }else{
-      // Use preference_id to show a modal
+      if (redirect) {
+        // use the URL if you want to redirect
+        console.log(`Redireccionar a la url: ${url}`)
+        window.location.href = url;
+      } else {
+        // Use preference_id to show a modal
         const mp = new MercadoPago(MERCADOPAGO_PUBLIC_KEY, {
           locale: 'es-AR'
         });
-  
-      // Inicializa el checkout
-      mp.checkout({
+
+        // Inicializa el checkout
+        mp.checkout({
           preference: {
             id: preference_id
           },
@@ -46,9 +46,38 @@ function get_preference_id(e) {
             container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
             label: 'Pagar', // Cambia el texto del botón de pago (opcional)
           }
-      }); 
-    }
+        });
+      }
 
-  });
+    });
 
+}
+
+var paypal_btn = document.getElementById("paypal");
+paypal_btn.addEventListener('click', get_preference_paypal);
+
+function get_preference_paypal(e) {
+  
+  // paso 1. Preparar el pago (ir al backend y obtener un preference_id)
+  // paso 2. Crear un botón que abre la ventana de MercadoPago.
+  e.preventDefault();
+  console.log("click");
+
+  const payment_url = `${base_url}/paypal/pago`;
+
+  const data = { "amount": 1234 }
+
+  fetch(payment_url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }
+  )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      //debugger
+      const url = data.href;
+      window.location.href = url;
+    });
 }
